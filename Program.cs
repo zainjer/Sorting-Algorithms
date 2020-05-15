1540b839c9cd9db942933ea37a814750838dbb1b
Areeb <arainareeb@gmail.com>
2020-05-16 03:42:33 +0500

    Implemented basic Cocktail shaker

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace SortingAlgorithms
 {
     class Program
     {
-        struct ListWithInfo{
+        private struct ListWithInfo{
             public int[] list;
             public long steps;
             public long passes;
@@ -19,26 +19,24 @@ namespace SortingAlgorithms
             }
 
         }
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
-            var unsortedList = GenerateRandomIntegers(10,0,1000); 
+            var unsortedList = GenerateRandomIntegers(5,0,1000); 
             
             PrintList(unsortedList);
+            //Console.WriteLine("< Unsorted");
 
-            var listInfo1 = BubbleSort_Optimization1(unsortedList);
-            var listInfo = BubbleSort(unsortedList);
-            var listInfo2 = BubbleSort_Optimization2(unsortedList);
+            var list2 = CocktailShakerSort(unsortedList);
+            var listInfo1 = BubbleSort(unsortedList);
+
             
-            System.Console.WriteLine("\tBubble Sort No Optimization:\n"+CreateResultString(listInfo));
-            PrintList(listInfo);
+            System.Console.WriteLine("\tBubble Sort No Optimization:\n"+CreateResultString(listInfo1));
 
-            System.Console.WriteLine("\tBubble Sort Optimzation 1:\n"+CreateResultString(listInfo1));
-            PrintList(listInfo1);
             
-            System.Console.WriteLine("\tBubble Sort Optimzation 2:\n"+CreateResultString(listInfo2));
-            PrintList(listInfo2);
+            System.Console.WriteLine("\tCocktailShaker Sort No Optimization:\n"+CreateResultString(list2));
 
-
+            
             System.Console.WriteLine("\nPress Enter to Continue");
             Console.ReadLine();
             Console.Clear();
@@ -138,12 +136,44 @@ namespace SortingAlgorithms
         static ListWithInfo CocktailShakerSort(int[] list)          //https://en.wikipedia.org/wiki/Cocktail_shaker_sort
         {
             var len = list.Length;
+            long swaps = 0;
             long steps = 0;
-                                            
-            throw new NotImplementedException();
-
+            long passes = 0;
+            
+            for(var i = 0; i < len; i++)
+            {
+                //forward direction 
+                for (var j = 0; j < len - 1; j++)
+                {
+                    if(list[j]>list[j+1])
+                    {
+                        list = Swap(list,j,j+1);
+                        swaps++;
+                    }
+                    steps++;
+                    
+                    //Debug code
+                    ////PrintList(list,true);
+                    ////Console.WriteLine(" < Forward"+i);
+                }
+                //reverse direction
+                for (var j = len-1; j >0; j--)
+                {
+                    if(list[j]<list[j-1])
+                    {
+                        list = Swap(list,j,j-1);
+                        swaps++;
+                    }
+                    steps++;
+                    
+                    //Debug code
+                    ////PrintList(list,true);
+                    ////Console.WriteLine(" < Backward"+i);
+                }
+                passes++;
+            }
+            return new ListWithInfo(list,steps,passes,swaps);
         }
-
         static int[] GenerateRandomIntegers(int length,int minValue,int maxValue)
         {
             var list = new List<int>();
@@ -163,9 +193,16 @@ namespace SortingAlgorithms
             foreach(var x in list)
                 System.Console.WriteLine(x);          
         }
+        static void PrintList(int[] list,bool horizontal)
+        {
+            Console.Write("[ ");
+            foreach(var x in list)
+                System.Console.Write(x+", ");          
+            Console.Write("]");
+        }
         static string CreateResultString(ListWithInfo listInfo)
         {
-            return $"Steps Taken:{listInfo.steps}  Passes:{listInfo.passes}  Swaps:{listInfo.swaps}";          
+            return $"Steps Taken:{listInfo.steps}  Passes:{listInfo.passes}  Swaps: {listInfo.swaps}";          
         }
         static int[] Swap(int[] list,int indexA,int indexB)
         {
